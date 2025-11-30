bb731f322d599e9b00ecd173b806ebb95d90736d
jojczykp <pawel.jojczyk@gmail.com>
2025-11-30 21:45:22 +0100

    Create topic on startup - move bask to lifecycle.postStart

--- kafka-chart/init-scripts/init.sh
+++ kafka-chart/init-scripts/init.sh
@@ -5,22 +5,20 @@ set -e
 echo "----------------------------------------"
 echo
 echo "Waiting for Kafka to be ready..."
-until /opt/kafka/bin/kafka-broker-api-versions.sh --bootstrap-server "${BOOTSTRAP_SERVER}"; do
+until /opt/kafka/bin/kafka-broker-api-versions.sh --bootstrap-server localhost:9094; do
   echo "Kafka not yet listening. Sleeping 1 second..."
   sleep 1
 done
-
-sleep 30
 
 echo "Kafka is ready."
 
 echo
 echo "Creating topics..."
-/opt/kafka/bin/kafka-topics.sh --bootstrap-server "${BOOTSTRAP_SERVER}" --create --topic test-topic-1 --partitions 1 --replication-factor 1
+/opt/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9094 --create --topic test-topic-1 --partitions 1 --replication-factor 1
 
 echo
 echo "All topics:"
-/opt/kafka/bin/kafka-topics.sh --bootstrap-server "${BOOTSTRAP_SERVER}" --list | sed 's/^/- /'
+/opt/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9094 --list | sed 's/^/- /'
 
 echo
 echo "----------------------------------------"
