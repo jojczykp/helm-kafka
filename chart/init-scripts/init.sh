bc918935a6b8f8df6e0b87346b220078dc87073d
jojczykp <pawel.jojczyk@gmail.com>
2026-02-02 00:24:14 +0100

    Multiple topics

--- chart/init-scripts/init.sh
+++ chart/init-scripts/init.sh
@@ -5,7 +5,8 @@ set -e
 echo "----------------------------------------"
 echo
 echo "Waiting for Kafka to be ready..."
-until /opt/kafka/bin/kafka-broker-api-versions.sh --bootstrap-server localhost:9095 2>/dev/null; do
+until /opt/kafka/bin/kafka-broker-api-versions.sh --bootstrap-server localhost:9095 2>/dev/null
+do
   echo "Kafka not yet listening. Sleeping 1 second..."
   sleep 1
 done
@@ -13,8 +14,14 @@ done
 echo "Kafka is ready."
 
 echo
-echo "Creating ${KAFKA_TOPIC} topic..."
-/opt/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9095 --create --if-not-exists --topic "${KAFKA_TOPIC}" --partitions 1 --replication-factor 1
+ORIGINAL_IFS=${IFS}
+IFS=','
+for KAFKA_TOPIC in ${KAFKA_TOPICS}
+do
+  echo "Creating ${KAFKA_TOPIC} topic..."
+  /opt/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9095 --create --if-not-exists --topic "${KAFKA_TOPIC}" --partitions 1 --replication-factor 1
+done
+IFS=${ORIGINAL_IFS}
 
 echo
 echo "All topics:"
